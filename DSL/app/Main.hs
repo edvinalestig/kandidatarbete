6569ef32cb8410f4e7d2e58a82e221b89fd07538
Torphage <filip.torphage1@gmail.com>
2022-03-03 17:30:32 +0100

    Wrote more on DSL, not yet done with first version
    Co-authored-by: OneZeroWasTaken <OneZeroWasTaken@users.noreply.github.com>
    Co-authored-by: luksch1121 <luksch1121@users.noreply.github.com>
    Co-authored-by: Edvin Alestig <edvinalestig@users.noreply.github.com>

--- DSL/app/Main.hs
+++ DSL/app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import DSL
 
 main :: IO ()
-main = someFunc
+main = play
--- DSL/src/DSL.hs
+++ DSL/src/DSL.hs
@@ -1,156 +1,169 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# LANGUAGE BlockArguments #-}
+
 module DSL where
 import Data.List (transpose, group)
 import Control.Monad.Random (evalRandIO, MonadRandom (getRandomR))
 import Data.Bifunctor (Bifunctor(bimap))
-import Data.List.Split (chunksOf)
+import Data.List.Split (chunksOf, splitOn)
+import Text.Read (readMaybe)
 -- data Game = Game [Equipment] [Rule]
 
 -- tic-tac-toe
 tictactoe :: Game
 tictactoe = Game {
     board = newBoard 3 3,
-    equipment = [
+    pieces = [
         Piece "X" (Player "X"),
         Piece "O" (Player "O")
     ],
+    dice = [],
     players = [
         Player "X",
         Player "O"
     ],
     rules = [
-        PlaceRules [const False]
+        PlaceRule undefined
     ],
-    winCondition = [
-        Con (const True)
-    ]
+    endConditions = EndCondition {
+        drawCondition = undefined,
+        winCondition = undefined
+    }
 }
 
 data Game = Game
-    { 
-        board :: Board String,
-        equipment :: [Equipment],
-        players :: [Player String],
-        rules :: [Rule],
-        winCondition :: [WinCondition]
+    {
+        board         :: Board,
+        pieces        :: [Piece],
+        dice          :: [Die],
+        -- equipment  :: (Equipment t) => [t],
+        players       :: [Player],
+        rules         :: [Rule],
+        endConditions :: EndCondition
     }
 
-data Equipment = Piece String (Player String) | Dice Int
---data Equipment where
-    --Board :: Int -> Int -> Equipment
-    --Piece :: t -> Player t -> Equipment
-    --Dice :: [Die] -> Equipment
 
-data Rule where
-    PlaceRules :: [Tile t -> Bool] -> Rule
-    LegalMoves :: [Tile t -> Tile t -> Bool] -> Rule
-    WinConditions :: [Board t -> Maybe (Player t)] -> Rule
-
-data RuleInternal where
-    PlaceRule :: TilePos t -> Board t -> (TilePos t -> Bool) -> RuleInternal
-    LegalMove :: TilePos t -> (TilePos t -> TilePos t -> Bool) -> RuleInternal
-    WinCondition :: TilePos t -> (Board t -> Maybe (Player t)) -> RuleInternal
+-- data Equipment where
+--     Piece' :: Piece g t -> Equipment -- (Show g, Show t) => g -> Player t -> Equipment -- Piece g (Player t) :: Equipment
+--     Dice   :: [Die] -> Equipment                              -- Dice [Die] :: Equipment
 
 
--- runEquipment :: Equipment -> Equipment 
--- runEquipment (Board a b) = undefined 
--- runEquipment (Piece t p) = undefined 
--- runEquipment (Dice ds) = undefined 
+newtype Rule = PlaceRule (Pos -> Board -> Bool)
+    --LegalMoves    :: [Tile t -> Tile t -> Board t -> Bool] -> Rule -- Needs changing?
+    --winConditions :: [Board -> Maybe Player]
 
-runRules :: RuleInternal -> RuleInternal
-runRules (PlaceRule t b f) = undefined
-runRules (LegalMove t f) = undefined
-runRules (WinCondition t f) = undefined
+data Pos = Pos Int Int 
 
+data EndCondition = EndCondition {
+    drawCondition :: ([Bool] -> Bool) -> [Board -> Bool],
+    winCondition  :: ([Bool] -> Bool) -> [Board -> Bool]
+}
 
-data WinCondition where
-    Con :: (Board t -> Bool) -> WinCondition
-
-boardSize :: Board t -> (Int, Int)
+boardSize :: Board -> (Int, Int)
 boardSize b = (length b, (length . chunksOf 3) b)
 
-newtype Player t = Player t deriving (Eq)
+newtype Player = Player String
+    deriving (Show, Eq)
 
-data Tile t = PlayerTile (Player t) | Empty deriving (Eq)-- MultiTile [Player t]
-data TilePos t = Pos Int Int (Tile t)
-type Board t = [TilePos t]
+data Piece = Piece String Player
+    deriving (Eq)
+data Tile = PieceTile Piece Int Int | Empty Int Int
+    deriving (Eq)
+
+type Board = [Tile]
 
 newtype Die = Die Int
 
 play :: IO ()
-play = undefined
+play = runGame tictactoe
 
-gameLoop :: Game -> IO (Player t)
-gameLoop g = do
-  let pl = head $ players g
-  putStrLn $ "Player " ++ show pl ++ "'s turn"
-  displayGame $ board g
+runGame :: Game -> IO ()
+runGame g = do
+    winner <- runGame' g
+    print winner
+    where
+        runGame' :: Game -> IO Player
+        runGame' game = do
+            let currPlayer = head $ players game
+            putStrLn $ "Player " ++ show currPlayer ++ "'s turn"
+            displayGame game
 
-  --putStrLn "Throw dice"
-  --moves <- evalRandIO die
-  --putStrLn $ "You rolled a " ++ show moves
-  --let piece      = pieceOfPlayer pl g
-      --newGs      = move (Increment piece moves) g
-      --afterRules = applyRules newGs
-  
---   let placeRules = findPlaceRules g
-  let placeRules = filter isPlaceRule $ rules g
-  input <- getValidInput placeRules
-  -- l <- getLine
-  -- check if input matches place rules
-  -- place piece on board
-  
-  --case checkWin afterRules of
-    --Just p -> do
-      --printGame afterRules
-      --return p
-    --Nothing -> gameLoop $ cyclePlayers afterRules
-  undefined
+            let placeRules' = rules game
+            input <- getValidInput placeRules' (board game)
 
-getValidInput :: [Rule] -> IO (Int, Int)
-getValidInput r = do
+            let piece = undefined
+            --placePiece input undefined b
+
+
+            runGame' $ game {players = cyclePlayers $ players game}
+
+
+
+-- gameLoop :: Game -> IO Player
+-- gameLoop g = do
+--   let pl = head $ players g
+--   putStrLn $ "Player " ++ show pl ++ "'s turn"
+--   --displayGame $ board g
+
+--   --putStrLn "Throw dice"
+--   --moves <- evalRandIO die
+--   --putStrLn $ "You rolled a " ++ show moves
+--   --let piece      = pieceOfPlayer pl g
+--       --newGs      = move (Increment piece moves) g
+--       --afterRules = applyRules newGs
+
+-- --   let placeRules = findPlaceRules g
+--   let placeRules = placeRules $ rules g
+--   input <- getValidInput placeRules
+--   -- l <- getLine
+--   -- check if input matches place rules
+--   -- place piece on board
+
+--   --case checkWin afterRules of
+--     --Just p -> do
+--       --printGame afterRules
+--       --return p
+--     --Nothing -> gameLoop $ cyclePlayers afterRules
+--   undefined
+
+getValidInput :: [Rule] -> Board -> IO (Int, Int)
+getValidInput r b = do
     putStrLn "Write valid input"
     input <- getLine
-    let (x, y) = bimap read read $ splitOn ',' input
+    let xs = filterNothing (map readMaybe $ splitOn "," input :: [Maybe Int])
+    if length xs /= 2 then
+        getValidInput r b
+    else do
+        let [x, y] = xs
+            valid = any (\(PlaceRule f) -> f (Pos x y) b) r
 
-    undefined
+        if valid then return (x, y) else getValidInput r b
+        -- case readMaybe (show (x,y)) :: Maybe (Int, Int) of
+        --     Just a 
 
-splitOn :: Char -> String -> (String,String)
-splitOn c str = (takeWhile (/=c) str, tail $ dropWhile (/=c) str)
+placePiece :: Piece -> Pos -> Board -> Board
+placePiece p pos b = undefined
 
-currentPlayer :: [Player t] -> Player t
-currentPlayer = head
+-- splitOn :: Char -> String -> (String,String)
+-- splitOn c str = (takeWhile (/=c) str, tail $ dropWhile (/=c) str)
 
-isPlaceRule :: Rule -> Bool
-isPlaceRule = \case (PlaceRules _) -> True ; _ -> False
+-- isPlaceRule :: Rule -> Bool
+-- isPlaceRule = \case (PlaceRules _) -> True ; _ -> False
 
-cyclePlayers :: [Player t] -> [Player t]
+filterNothing :: [Maybe a] -> [a]
+filterNothing []     = []
+filterNothing (x:xs) = case x of
+    Nothing -> filterNothing xs
+    Just a  -> a : filterNothing xs
+
+cyclePlayers :: [Player] -> [Player]
 cyclePlayers ps = tail ps ++ [head ps]
 
 
-displayGame :: Board t -> IO ()
+displayGame :: Game -> IO ()
 displayGame = undefined
-
-
-
--- prettyPrint :: Board t -> IO ()
--- prettyPrint b = do
---     putStrLn $ replicate (1 + 4 * length (head b)) '-'
---     prettyPrint' $ (map f) b
-
---     where
---         f :: TilePos t -> String
---         f t = case t of
---             Pos _ _ Empty -> " "
---             Pos _ _ s     -> show s
-
---         prettyPrint' :: [String] -> IO ()
---         prettyPrint' [] = return ()
---         prettyPrint' (b:bs)  = do
---             putStrLn $ foldl (\s t -> s ++ t ++ " | ") "| " b
---             putStrLn $ replicate (1 + 4 * length b) '-'
---             prettyPrint' bs
 
 
 -- * Utility functions
@@ -158,57 +171,10 @@ displayGame = undefined
 throwDie :: Die -> IO Int
 throwDie (Die n) = evalRandIO $ getRandomR (1,n)
 
-newBoard :: Int -> Int -> Board String
-newBoard w h = [Pos x y Empty | y <- [0..h-1], x <- [0..w-1]]
-
-
---getRow :: Board t -> Int -> [Tile t]
---getRow b n = getRows b !! n
-
--- | Return the board as a list of rows
---getRows :: Board t -> Board t
---getRows = getColumns . transpose
-
---getColumn :: Board t -> Int -> [Tile t]
---getColumn b n = getColumns b !! n
-
-getColumns :: Board t -> Board t
-getColumns = id
-
-
---checkWin :: (Eq t) => Board t -> Bool
---checkWin b = winningRow b || winningColumn b || winningDiagonal b
-
---tie :: (Eq t) => Board t -> Bool 
---tie = all (notElem Empty)
-
---winningRow :: (Eq t) => Board t -> Bool
---winningRow = winningColumn . transpose
-
---winningColumn :: (Eq t) => Board t -> Bool
---winningColumn b = or [fourSame col | col<-b]
-
---winningDiagonal :: (Eq t) => Board t -> Bool
---winningDiagonal b =
-   --any fourSame (getDiagonal1 b) || any fourSame (getDiagonal2 b)
-
--- stulen från devin
---getDiagonal1 :: Board t -> Board t
---getDiagonal1 b = concat [[[(b !! (y + k')) !! (x + k') | k' <- [0..3]]
-                           -- | x <- [0..length (head b) - 4]]
-                           -- | y <- [0..length b - 4]] 
-
-
---getDiagonal2 :: Board t -> Board t
---getDiagonal2 = getDiagonal1 . reverse
+newBoard :: Int -> Int -> Board
+newBoard w h = [Empty x y | y <- [0..h-1], x <- [0..w-1]]
 
 -- * behöver ändras
-fourSame :: (Eq t) => [Tile t] -> Bool
-fourSame col = any (\x -> length x >= 4 && Empty `notElem` x) (group col)
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
-
-
-
+--fourSame :: (Eq t) => [Tile t] -> Bool
+--fourSame col = any (\x -> length x >= 4 && Empty `notElem` x) (group col)
 
